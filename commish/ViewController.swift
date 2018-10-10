c4cadfe89de1bd1a77b06573dd7ea1b57deb6b98
Ryan Kaiser <rkaiser0176@jh-mba158-12.d214.org>
2018-10-10 14:41:29 -0500

    added if let statement

--- commish/ViewController.swift
+++ commish/ViewController.swift
@@ -12,18 +12,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var totalPayLabel: UILabel!
     
-    @IBOutlet weak var comissionPayTextField: UITextField!
+    @IBOutlet weak var commissionPayTextField: UITextField!
     override func viewDidLoad() {
         let basePay:Int = 500
         super.viewDidLoad()
     }
 
     @IBAction func calculateOnTapped(_ sender: Any) {
-    }
     
+        getInput()
     
 }
     func getInput(){
+        if let data = commissionPayTextField.text, let commisionPay = Int(data){
+            print("everything is okay")
+        } else {
+            print("error occured")
+        }
+        
+        
 }
 
+
+
+
+}
 
